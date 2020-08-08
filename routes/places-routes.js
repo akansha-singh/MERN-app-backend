9f20d7564ea3836407f6054ee1fb9d06784e7046
akansha-singh <singhakansha281@gmail.com>
2020-08-08 12:32:01 +0530

    Finalizing the Get Places by User ID Resource

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const placeControllers = require('../controllers/places-controllers');
 
 router.get('/:pid', placeControllers.getPlaceById);
-router.get('/user/:uid', placeControllers.getPlaceByUserId);
+router.get('/user/:uid', placeControllers.getPlacesByUserId);
 router.post('/', placeControllers.createPlace);
 router.patch('/:pid',placeControllers.updatePlace);
 router.delete('/:pid',placeControllers.deletePlace);
